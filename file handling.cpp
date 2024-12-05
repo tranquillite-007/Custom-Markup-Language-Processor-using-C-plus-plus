f4aec71c876a8c667a9f08003a04b8174682802c
tranquillite-007 <sinhaaman655@gmail.com>
2024-12-05 22:19:51 +0530

    update 05-12-24

--- file handling.cpp
+++ file handling.cpp
@@ -7,31 +7,50 @@ int main(){
 	fstream inputFile;
 	
 	
-	// Open input.txt file
-	inputFile.open("input.txt", ios::out);
-	if(inputFile.is_open()){
-		
-		//add initial data in text file 
-		
-		inputFile <<"Add your commands here....  \n";
-		
-		
-				
+	// Open input.txt file in append mode
+	inputFile.open("input.txt", ios::app);
+	if(inputFile.is_open()){				
 		inputFile.close();
 	}
 	
 	
 	
 	
+    //code for output file or for converting txt to html 	
+	fstream outputFile;
 	
-//	fstream outputFile;
-//	inputFile.open("input.txt", ios::in);
-//	outputFile.open("index.html", ios::out);
-//	if(outputFile.is_open()){
-//		while(getline(inputFile, line)){
-//			outputFile << line << "<br> \n";
-//			cout<<"the file is created \n";
-//		}
-//	}
+	//open input.txt file for read mode
+	inputFile.open("input.txt", ios::in);
+	
+	//open index.html file in read mode
+	outputFile.open("index.html", ios::out);
+	if(outputFile.is_open()){
+		
+		//initial or predefined lines before body tag in html file 
+		outputFile << "<!DOCTYPE html> \n";
+		outputFile << "<html lang=\"en\"> \n";
+		outputFile << " <head> \n";
+		outputFile << "    <meta charset=\"UTF-8\"> \n";
+		outputFile << "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"> \n";
+		
+		// later i'll add title programme to add title in website 
+		outputFile << "    <title></title> \n";
+		outputFile << " </head> \n";
+		outputFile << " <body> \n";
+		
+		//while loop to read all lines from inputFile.txt
+		while(getline(inputFile, line)){
+			
+			// test statement to print h1 tag 
+//			outputFile <<" " << line << " \n";
+			outputFile <<"    <h1 style=\"width: max-content; margin: auto; background-color: aqua;\">" << line << "</h1> \n";
+
+			cout<<"congo developer!! the file is created and working properly...  \n";
+		}
+		
+		//initial or predefined lines after html body in html file
+		outputFile <<" </body> \n";
+		outputFile <<"</html> \n";
+	}
 	return 0;
 }
