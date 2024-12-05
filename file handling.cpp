07e37f5af151f80aa5a7f2e3d85fc62ab7fc3e68
tranquillite-007 <sinhaaman655@gmail.com>
2024-12-05 21:41:27 +0530

    update 05-12-24

--- file handling.cpp
+++ file handling.cpp
@@ -4,26 +4,34 @@
 using namespace std;
 int main(){
 	string line;
-	fstream myFile;
-	myFile.open("file.txt", ios::out);
-	if(myFile.is_open()){
-		myFile <<" first line \n";
-		myFile.close();
-	}
-	myFile.open("file.txt", ios::app);
-	if(myFile.is_open()){
-		myFile << " second appended line  ! \n";
-		myFile.close();
-	}
-	fstream newFile;
-	myFile.open("file.txt", ios::in);
-	newFile.open("file.html", ios::out);
-	if(newFile.is_open()){
-		while(getline(myFile, line)){
-			newFile << line << "<br> \n";
-			cout<<"the file is created \n";
-		}
+	fstream inputFile;
+	
+	
+	// Open input.txt file
+	inputFile.open("input.txt", ios::out);
+	if(inputFile.is_open()){
 		
+		//add initial data in text file 
+		
+		inputFile <<"Add your commands here....  \n";
+		
+		
+				
+		inputFile.close();
 	}
+	
+	
+	
+	
+	
+//	fstream outputFile;
+//	inputFile.open("input.txt", ios::in);
+//	outputFile.open("index.html", ios::out);
+//	if(outputFile.is_open()){
+//		while(getline(inputFile, line)){
+//			outputFile << line << "<br> \n";
+//			cout<<"the file is created \n";
+//		}
+//	}
 	return 0;
 }
