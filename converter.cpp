c8e2c0e6ab56537c93bad87efcfb2f25f752af3e
tranquillite-007 <sinhaaman655@gmail.com>
2024-12-08 00:28:06 +0530

    update 08-12-24

--- converter.cpp
+++ converter.cpp
@@ -117,13 +117,24 @@ int main() {
                 }
                 else if(firstWord == "$image"){
                 	size_t start = content.find('(');
-                	size_t endUrl = content.find(',');
+                	size_t delimiter_1 = content.find(',');
                 	size_t end = content.find(')');
-                	if(start != string::npos && endUrl != string::npos && end != string::npos && end > start){
-                		string imageUrl = content.substr(start + 1, endUrl - start -1);
-                		string alt = content.substr(endUrl + 1, end - endUrl - 1);
+                	if(start != string::npos && delimiter_1 != string::npos && end != string::npos && end > start){
+                		string imageUrl = content.substr(start + 1, delimiter_1 - start -1);
+                		string alt = content.substr(delimiter_1 + 1, end - delimiter_1 - 1);
                 		outputFile <<  "<img src=\"" <<imageUrl <<"\" alt=\"" <<alt <<"\" /> \n";  //<img src="" alt="">
                 		cout << "Image url:" << imageUrl << "Image Alt:" << alt << endl;  //print image info on terminal
+					}
+				}
+				else if(firstWord == "$link"){
+					size_t start = content.find('(');
+					size_t end = content.find(')');
+					size_t delimiter_1 = content.find(',');
+					if(start != string::npos && end != string::npos && delimiter_1 != string::npos && end > start){
+						string linkUrl = content.substr(start + 1, delimiter_1 -start -1 );
+						string hypertext = content.substr(delimiter_1 +1 , end - delimiter_1 -1);
+						outputFile << "<a href=\"" << linkUrl << "\">" << hypertext << "</a>"; //<a href=""></a>
+						cout << "Link:" << linkUrl << "  Hyperlink:" << hypertext << endl; //print link information in terminal
 					}
 				}
             }
