99dcf6b2b3e39d569492107f8ac9b555084e9691
tranquillite-007 <sinhaaman655@gmail.com>
2024-12-22 19:24:22 +0530

    update 22-12-24 (structural tag complete)

--- language version 1.0.1/converter.cpp
+++ language version 1.0.1/converter.cpp
@@ -1,25 +1,45 @@
-#include<iostream>
-#include<fstream>
-#include<string>
-#include<sstream>
-#include<algorithm>
-#include<vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int main(){
-	string line;
-	fstream inputFile, outputFile;
-	
-	inputFile.open("userInput.txt", ios::in);
-	if(inputFile.peek() == ifstream::traits_type::eof()){
-		inputFile.close();
-		
-		inputFile.open("userInput.txt", ios::app);
-		if(inputFile.is_open()){
-			// Instructions to write in txt file
+
+
+
+
+string processTilde(const string& input) {
+    string result;
+    for (size_t i = 0; i < input.length(); ++i) {
+        if (input[i] == '~') {
+            if (i + 1 < input.length() && (input[i + 1] == '(' || input[i + 1] == ')' || input[i + 1] == '[' || input[i + 1] == ']')) {
+                continue;
+            } else {
+                result += input[i];
+            }
+        } else {
+            result += input[i];
+        }
+    }
+    return result;
+}
+
+int main() {
+    string line;
+    fstream inputFile, outputFile;
+
+    inputFile.open("userInput.txt", ios::in);
+    if (inputFile.peek() == ifstream::traits_type::eof()) {
+        inputFile.close();
+
+        inputFile.open("userInput.txt", ios::app);
+        if (inputFile.is_open()) {
+            // Instructions to write in txt file
             inputFile << "<!-- Hello Developer, this project is created by Aman Sinha you can contact me on sinhaaman655@hotmail.com | follow and star the repo on www.github.com/tranquillite-007--> \n";
-            inputFile << "<!-- About this project : This is a C++ project to convert or we can say that a project to create HTML, CSS website by just writing some lines in normal English--> \n";    
+            inputFile << "<!-- About this project : This is a C++ project to convert or we can say that a project to create HTML, CSS website by just writing some lines in normal English--> \n";
             inputFile << "<!-- Direction to use this project: --> \n";
             inputFile << "<!-- Write the statements for your website based on below rules --> \n";
             inputFile << "<!-- 1. Write the statement in as shown below: --> \n";
@@ -39,109 +59,244 @@ int main(){
             inputFile << "<!-- you can remove above lines or comments from txt file, these are non-functional lines -->";
             inputFile << "<!-- Good Luck (Happy Coding) --> \n";
             inputFile.close();
-		}
-	}else{
-		inputFile.close(); //close if the file is not empty
-	}
+        }
+    } else {
+        inputFile.close(); //close if the file is not empty
+    }
 
-	inputFile.open("userInput.txt", ios::in);
-	outputFile.open("output.html", ios::out);
-	
-	if(outputFile.is_open()){
-		// First add HTML snippet to our .html file
-        outputFile << "<!DOCTYPE html> \n";
+    inputFile.open("userInput.txt", ios::in);
+    outputFile.open("output.html", ios::out);
+
+    if (outputFile.is_open()) {
+        // First add HTML snippet to our .html file outputFile << "<!DOCTYPE html> \n";
         outputFile << "<html lang=\"en\"> \n";
-        outputFile << " <head> \n";
+        outputFile << "<head> \n";
         outputFile << "    <meta charset=\"UTF-8\"> \n";
         outputFile << "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"> \n";
         
         // Later I'll add title program to add title in website 
         outputFile << "    <title></title> \n";
         outputFile << "    <link href=\"https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css\" rel=\"stylesheet\"> \n";
-        outputFile << " </head> \n";
-        outputFile << " <body> \n";
-        
-        
-        
-        while(getline(inputFile, line)){
-        	if(line.empty() || line[0] != '$'){
-        		continue;
-			}else{
-				string content = line;
+        outputFile << "</head> \n";
+        outputFile << "<body> \n";
+
+        while (getline(inputFile, line)) {
+            if (line.empty() || line[0] != '$') {
+                continue;
+            } else {
+                string content = line;
+
+                istringstream iss(content);
+                string firstWord;
+                iss >> firstWord;
+                
+                size_t cStart = content.find('[');
+        		size_t cEnd = content.find(']');
+
+        		string className;
+        		if (cStart != string::npos && cEnd != string::npos && cEnd > cStart) {
+        		    className = processTilde(content.substr(cStart + 1, cEnd - cStart - 1));
+        		}   
 				
-				istringstream iss(content);
-				string firstWord;
+				//document structural tags first 
+				//paragraph
+				if (firstWord == "$paragraph") {
+            		size_t start = content.find('(');
+            		size_t end = content.find(')');
+            		if (start != string::npos && end != string::npos && end > start) {
+            		    string text = processTilde(content.substr(start + 1, end - start - 1));
+            		    if (!className.empty()) {
+            		        outputFile << "    <p class=\"" << className << "\">" << text << "</p> \n";
+            		    } else {
+            		        outputFile << "    <p>" << text << "</p> \n"; // No class attribute
+            		    }
+            		}
+        		}
+        		
+        		//headings from h1-h6
+        		//h1
+        		else if (firstWord == "$heading1") {
+            		size_t start = content.find('(');
+            		size_t end = content.find(')');
+            		if (start != string::npos && end != string::npos && end > start) {
+            		    string text = processTilde(content.substr(start + 1, end - start - 1));
+            		    if (!className.empty()) {
+            		        outputFile << "    <h1 class=\"" << className << "\">" << text << "</h1> \n";
+            		    } else {
+            		        outputFile << "    <h1>" << text << "</h1> \n"; 
+            		    }
+            		}
+        		}
+        		
+        		//h2
+        		else if (firstWord == "$heading2") {
+            		size_t start = content.find('(');
+            		size_t end = content.find(')');
+            		if (start != string::npos && end != string::npos && end > start) {
+            		    string text = processTilde(content.substr(start + 1, end - start - 1));
+            		    if (!className.empty()) {
+            		        outputFile << "    <h2 class=\"" << className << "\">" << text << "</h2> \n";
+            		    } else {
+            		        outputFile << "    <h2>" << text << "</h2> \n"; 
+            		    }
+            		}
+        		}
+        		
+        		//h3
+        		else if (firstWord == "$heading3") {
+            		size_t start = content.find('(');
+            		size_t end = content.find(')');
+            		if (start != string::npos && end != string::npos && end > start) {
+            		    string text = processTilde(content.substr(start + 1, end - start - 1));
+            		    if (!className.empty()) {
+            		        outputFile << "    <h3 class=\"" << className << "\">" << text << "</h3> \n";
+            		    } else {
+            		        outputFile << "    <h3>" << text << "</h3> \n"; 
+            		    }
+            		}
+        		}
+        		
+        		//h4
+        		else if (firstWord == "$heading4") {
+            		size_t start = content.find('(');
+            		size_t end = content.find(')');
+            		if (start != string::npos && end != string::npos && end > start) {
+            		    string text = processTilde(content.substr(start + 1, end - start - 1));
+            		    if (!className.empty()) {
+            		        outputFile << "    <h4 class=\"" << className << "\">" << text << "</h4> \n";
+            		    } else {
+            		        outputFile << "    <h4>" << text << "</h4> \n"; 
+            		    }
+            		}
+        		}
+        		
+        		//h5
+        		else if (firstWord == "$heading5") {
+            		size_t start = content.find('(');
+            		size_t end = content.find(')');
+            		if (start != string::npos && end != string::npos && end > start) {
+            		    string text = processTilde(content.substr(start + 1, end - start - 1));
+            		    if (!className.empty()) {
+            		        outputFile << "    <h5 class=\"" << className << "\">" << text << "</h5> \n";
+            		    } else {
+            		        outputFile << "    <h5>" << text << "</h5> \n"; 
+            		    }
+            		}
+        		}
+        		
+        		//h6
+        		else if (firstWord == "$heading6") {
+            		size_t start = content.find('(');
+            		size_t end = content.find(')');
+            		if (start != string::npos && end != string::npos && end > start) {
+            		    string text = processTilde(content.substr(start + 1, end - start - 1));
+            		    if (!className.empty()) {
+            		        outputFile << "    <h6 class=\"" << className << "\">" << text << "</h6> \n";
+            		    } else {
+            		        outputFile << "    <h6>" << text << "</h6> \n"; 
+            		    }
+            		}
+        		}
+        		
+        		//header
+				else if (firstWord == "$header_start") {
+        		    if (!className.empty()) {
+        		        outputFile << "<header class=\"" << className << "\">\n";
+        		    } else {
+        		        outputFile << "<header>\n";
+        		    }
+        		}
+				else if (firstWord == "$header_end") {
+        		    outputFile << "</header>\n";
+        		}
+        		
+        		//nav
+        		else if (firstWord == "$nav_start") {
+        		    if (!className.empty()) {
+        		        outputFile << "<nav class=\"" << className << "\">\n";
+        		    } else {
+        		        outputFile << "<nav>\n";
+        		    }
+        		}
+				else if (firstWord == "$nav_end") {
+        		    outputFile << "</nav>\n";
+        		}
+        		
+        		//main
+        		else if (firstWord == "$main_start") {
+        		    if (!className.empty()) {
+        		        outputFile << "<main class=\"" << className << "\">\n";
+        		    } else {
+        		        outputFile << "<main>\n";
+        		    }
+        		}
+				else if (firstWord == "$main_end") {
+        		    outputFile << "</main>\n";
+        		}
+        		
+        		//article
+        		else if (firstWord == "$article_start") {
+        		    if (!className.empty()) {
+        		        outputFile << "<article class=\"" << className << "\">\n";
+        		    } else {
+        		        outputFile << "<article>\n";
+        		    }
+        		}
+				else if (firstWord == "$article_end") {
+        		    outputFile << "</article>\n";
+        		}
+        		
+        		//aside
+        		else if (firstWord == "$aside_start") {
+        		    if (!className.empty()) {
+        		        outputFile << "<aside class=\"" << className << "\">\n";
+        		    } else {
+        		        outputFile << "<aside>\n";
+        		    }
+        		}
+				else if (firstWord == "$aside_end") {
+        		    outputFile << "</aside>\n";
+        		}
+        		
+        		//div
+        		else if (firstWord == "$div_start") {
+        		    if (!className.empty()) {
+        		        outputFile << "    <div class=\"" << className << "\">\n";
+        		    } else {
+        		        outputFile << "    <div>\n";
+        		    }
+        		}
+				else if (firstWord == "$div_end") {
+        		    outputFile << "    </div>\n";
+        		}
+        		
+        		//section
+        		else if (firstWord == "$section_start") {
+        		    if (!className.empty()) {
+        		        outputFile << "<section class=\"" << className << "\">\n";
+        		    } else {
+        		        outputFile << "<section>\n";
+        		    }
+        		}
+				else if (firstWord == "$section_end") {
+        		    outputFile << "</section>\n";
+        		} 
 				
-				iss >> firstWord; 
-				
-				//check the first word and operate based on there syntax structure
-				if (firstWord == "$word" || firstWord == "$div") {
-                    
-					size_t start = string::npos;
-					size_t end = string::npos;
-					size_t cStart = string::npos;
-					size_t cEnd = string::npos;
-					
-					int openParentheses = 0;
-					int openBrackets = 0;	
-                    	
-                    for (size_t i = 0; i < content.length(); ++i) {
-                        if (content[i] == '~') {
-                            // Skip the next character
-                            ++i;
-                            continue;
-                        }
-                        if (content[i] == '(') {
-                            if (openParentheses == 0) {
-                                start = i; 
-                            }
-                            openParentheses++;
-                        } else if (content[i] == ')') {
-                            openParentheses--;
-                            if (openParentheses == 0 && start != string::npos) {
-                                end = i; 
-                            }
-                        } else if (content[i] == '[') {
-                            if (openBrackets == 0) {
-                                cStart = i;
-                            }
-                            openBrackets++;
-                        } else if (content[i] == ']') {
-                            openBrackets--;
-                            if (openBrackets == 0 && cStart != string::npos) {
-                                cEnd = i; 
-                            }
-                        }
-                    }
-					
-					if (start != string::npos && end != string::npos && end > start){
-						string className;
-						
-						// Check if the class is there or not, if class info is available
-                        if (cStart != string::npos && cEnd != string::npos && cEnd > cStart) {
-                            className = content.substr(cStart + 1, cEnd - cStart - 1);
-                        } else {
-                            className = ""; // Default class name if not present
-                        }
-                        
-                         
-                        if (firstWord == "$word") {
-                            string text = content.substr(start + 1, end - start - 1);
-                            if (!className.empty()) {
-                                outputFile << "    <h1 class=\"" << className << "\">" << text << "</h1> \n";
-                                cout<< "working \n";
-                            } else {
-                                outputFile << "    <h1>" << text << "</h1> \n"; // No class attribute
-                                cout<< "no class working \n";
-                            }
-                        }
-					}	
-				}
-				
-			}
-		}
-		// html snippet lines after html body in html file
-        outputFile << " </body> \n";
+				//footer
+				else if (firstWord == "$footer_start") {
+        		    if (!className.empty()) {
+        		        outputFile << "<footer class=\"" << className << "\">\n";
+        		    } else {
+        		        outputFile << "<footer>\n";
+        		    }
+        		}
+				else if (firstWord == "$footer_end") {
+        		    outputFile << "</footer>\n";
+        		}        
+            }
+        }
+        // HTML snippet lines after HTML body in HTML file
+        outputFile << "</body> \n";
         outputFile << "</html> \n";
-	}	
+    }
 }
